6a980327a75c86a883020a9ae84477787c147571
Volkan Atalan <volkanatalan35@gmail.com>
2020-09-23 16:36:21 +0300

    new colors

--- src/values/colors.js
+++ src/values/colors.js
@@ -8,6 +8,11 @@ const colors = {
   seperatorGray: '#ededed',
   seperatorAppColor: '#d7fce7',
   shadow: '#000',
+  antrasit: '#293133',
+  aluminum: '#d6d6d6',
+  antiquewhite: '#faebd7',
+  antiflashwhite: '#f2f3f4',
+  blackchocolate: '#1b1811',
 }
 
 export default colors
